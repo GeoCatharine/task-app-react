f9891e9eb1a94fdc8039f1f0f4aa65aca35d6707
GeoCatharine <v.nahornakova@gmail.com>
2022-01-03 15:39:57 +0100

    added function for deleting tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,13 @@
+import { Fragment } from "react"
 import Task from "./Task"
 
-const Tasks = ({ tasks }) => {
-
+const Tasks = ({ tasks, onDelete }) => {
   return (
-    <>
+    <Fragment>
       {tasks.map((task) => (
-        <Task key={task.id} task={task}/>
+        <Task key={task.id} task={task} onDelete= {onDelete} />
       ))}
-    </>
+    </Fragment>
   )
 }
 
