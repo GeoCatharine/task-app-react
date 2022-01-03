2c157748fd8dbd8180bd796cfc1dc50dd28817bd
GeoCatharine <v.nahornakova@gmail.com>
2022-01-03 16:24:41 +0100

    added Add Task form, implemented logic for adding tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState (true)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,19 +26,35 @@ function App() {
     },
   ])
 
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // toggle Reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks 
           tasks={tasks} 
-          onDelete={deleteTask} />
+          onDelete={deleteTask} 
+          onToggle={toggleReminder} />
         ) : (
           'No tasks today'
         )}
