b04a9082b87cc9cdee9a8d328c92e89ad3afa1a2
GeoCatharine <v.nahornakova@gmail.com>
2022-01-04 10:46:10 +0100

    router added for about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState (false)
@@ -24,12 +27,27 @@ function App() {
     return data
   }
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    }) 
+      
+    const data = await res.json()
+    setTasks([...tasks, data])
   }
 
   // Delete Task
@@ -44,23 +62,47 @@ function App() {
 
   // toggle Reminder
 
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd= {showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? (
-        <Tasks 
-          tasks={tasks} 
-          onDelete={deleteTask} 
-          onToggle={toggleReminder} />
-        ) : (
-          'No tasks today'
-        )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd= {showAddTask}/>
+        <Routes>
+          <Route path='/' exact element={
+            <Fragment>
+              {showAddTask && <AddTask onAdd={addTask}/>}
+              {tasks.length > 0 ? (
+                <Tasks 
+                  tasks={tasks} 
+                  onDelete={deleteTask} 
+                  onToggle={toggleReminder} />
+                ) : (
+                  'No tasks today'
+              )}
+            </Fragment>
+          } />
+          <Route path='/about' element={< About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
